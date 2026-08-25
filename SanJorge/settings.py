0e65fe0eecfb2aeea8f764b25fe2f6e8a152dec6
soyJmujica <master1495@gmail.com>
2026-08-24 22:09:59 -0400

    Segundo comentario, actualizado Debug a 'RENDER'

--- SanJorge/settings.py
+++ SanJorge/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*id)crl_)7k5(tud$+3fo!i=p&=$co7t1g=p2u4p#y$c3s$317'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']
 
